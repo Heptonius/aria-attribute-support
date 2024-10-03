b2f9a8f930f4628660f06a2bcc7c371258cf1c95
Heptonius <keharacek@gmail.com>
2024-10-03 08:49:28 +0200

    add support for few initial aria roles

--- support.test.js
+++ support.test.js
@@ -1,50 +1,112 @@
-import { describe, it, expect } from 'vitest';
-import { getSupportedAriaAttributes } from './support';
+import { describe, it, expect } from "vitest";
+import { getSupportedAriaAttributes } from "./support";
 
-describe('getSupportedAriaAttributes', () => {
-    it('should return an empty array for any input', () => {
-        expect(getSupportedAriaAttributes('button')).toEqual([]);
-        expect(getSupportedAriaAttributes('link')).toEqual([]);
-        expect(getSupportedAriaAttributes('checkbox')).toEqual([]);
+describe("getSupportedAriaAttributes", () => {
+  it("should return an empty array for any input", () => {
+    expect(getSupportedAriaAttributes("button")).toEqual([]);
+    expect(getSupportedAriaAttributes("link")).toEqual([]);
+    expect(getSupportedAriaAttributes("checkbox")).toEqual([]);
+  });
+
+  it("should return an empty array for undefined input", () => {
+    expect(getSupportedAriaAttributes(undefined)).toEqual([]);
+  });
+
+  it("should return an empty array for null input", () => {
+    expect(getSupportedAriaAttributes(null)).toEqual([]);
+  });
+
+  it("should return an empty array for empty string input", () => {
+    expect(getSupportedAriaAttributes("")).toEqual([]);
+  });
+
+  describe("returns correct list of ARIA attributes for a given ARIA role", () => {
+    it('when the ARIA role is "alert"', () => {
+      const ariaRole = "alert";
+
+      const expectedAttributes = [
+        "aria-atomic",
+        "aria-busy",
+        "aria-controls",
+        "aria-current",
+        "aria-describedby",
+        "aria-details",
+        "aria-disabled", // deprecated on this role in ARIA 1.2
+        "aria-dropeffect",
+        "aria-errormessage", // deprecated on this role in ARIA 1.2
+        "aria-flowto",
+        "aria-grabbed",
+        "aria-haspopup", // deprecated on this role in ARIA 1.2
+        "aria-hidden",
+        "aria-invalid", // deprecated on this role in ARIA 1.2
+        "aria-keyshortcuts",
+        "aria-label",
+        "aria-labelledby",
+        "aria-live",
+        "aria-owns",
+        "aria-relevant",
+        "aria-roledescription",
+      ];
+
+      expect(getSupportedAriaAttributes(ariaRole)).toEqual(expectedAttributes);
     });
+    it('when the ARIA role is "alertdialog"', () => {
+      const ariaRole = "alertdialog";
 
-    it('should return an empty array for undefined input', () => {
-        expect(getSupportedAriaAttributes(undefined)).toEqual([]);
+      const expectedAttributes = [
+        "aria-atomic",
+        "aria-busy",
+        "aria-controls",
+        "aria-current",
+        "aria-describedby",
+        "aria-details",
+        "aria-disabled", // deprecated on this role in ARIA 1.2
+        "aria-dropeffect",
+        "aria-errormessage", // deprecated on this role in ARIA 1.2
+        "aria-flowto",
+        "aria-grabbed",
+        "aria-haspopup", // deprecated on this role in ARIA 1.2
+        "aria-hidden",
+        "aria-invalid", // deprecated on this role in ARIA 1.2
+        "aria-keyshortcuts",
+        "aria-label",
+        "aria-labelledby",
+        "aria-live",
+        "aria-owns",
+        "aria-relevant",
+        "aria-roledescription",
+      ];
+
+      expect(getSupportedAriaAttributes(ariaRole)).toEqual(expectedAttributes);
     });
+    it('when the ARIA role is "input"', () => {
+      const ariaRole = "input";
 
-    it('should return an empty array for null input', () => {
-        expect(getSupportedAriaAttributes(null)).toEqual([]);
+      const expectedAttributes = [
+        "aria-atomic",
+        "aria-busy ",
+        "aria-controls",
+        "aria-current ",
+        "aria-describedby",
+        "aria-details",
+        "aria-disabled ",
+        "aria-dropeffect",
+        "aria-errormessage",
+        "aria-flowto",
+        "aria-grabbed ",
+        "aria-haspopup",
+        "aria-hidden ",
+        "aria-invalid ",
+        "aria-keyshortcuts",
+        "aria-label",
+        "aria-labelledby",
+        "aria-live",
+        "aria-owns",
+        "aria-relevant",
+        "aria-roledescription",
+      ];
+
+      expect(getSupportedAriaAttributes(ariaRole)).toEqual(expectedAttributes);
     });
-
-    it('should return an empty array for empty string input', () => {
-        expect(getSupportedAriaAttributes('')).toEqual([]);
-    });
-
-    describe("aria roles aria attribute support", () => {
-        it('should return an array of supported aria attributes for input role', () => {
-            expect(getSupportedAriaAttributes('input')).toEqual([
-                "aria-atomic",
-                "aria-busy ",
-                "aria-controls",
-                "aria-current ",
-                "aria-describedby",
-                "aria-details",
-                "aria-disabled ",
-                "aria-dropeffect",
-                "aria-errormessage",
-                "aria-flowto",
-                "aria-grabbed ",
-                "aria-haspopup",
-                "aria-hidden ",
-                "aria-invalid ",
-                "aria-keyshortcuts",
-                "aria-label",
-                "aria-labelledby",
-                "aria-live",
-                "aria-owns",
-                "aria-relevant",
-                "aria-roledescription",
-            ]);
-        });
-    })
+  });
 });
